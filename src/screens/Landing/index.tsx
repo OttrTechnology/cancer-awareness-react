29d642de1c562c4df9042abaf87dc43a4f830dec
Nirwan Rai <nirwanrai6969@gmail.com>
2023-09-12 15:30:33 +0545

    🚑 Quickfix: review changes

--- src/screens/Landing/index.tsx
+++ src/screens/Landing/index.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useRef } from "react";
 
-import LandingpageArrow from "assets/LandingpageArrow.svg";
-import LandingLogo from "assets/LandingLogo.svg";
+import LandingpageArrow from "assets/homepageArrow.svg";
+import LandingLogo from "assets/homeLogo.svg";
 
 import styles from "./index.module.scss";
 
@@ -12,7 +12,7 @@ import {
   BiLogoLinkedin,
 } from "react-icons/bi";
 
-import datas from "../Quiz/cancer-findings-data.json";
+import data from "../Quiz/cancer-findings-data.json";
 
 import {
   Bodies,
@@ -25,6 +25,7 @@ import {
 } from "matter-js";
 
 import { useGameContext } from "hooks";
+import clsx from "clsx";
 
 export const Landing = () => {
   const { setActiveScreen } = useGameContext();
@@ -33,7 +34,7 @@ export const Landing = () => {
   const engine = useRef(Engine.create());
   const scene = useRef(null);
 
-  const changePage = () => {
+  const startQuiz = () => {
     setActiveScreen("QUIZ");
   };
 
@@ -52,6 +53,8 @@ export const Landing = () => {
         background: "transparent",
       },
     });
+
+    // engine.current.gravity.scale = 0.001;
 
     Render.lookAt(render, {
       min: { x: 0, y: 0 },
@@ -81,29 +84,22 @@ export const Landing = () => {
       }),
     ]);
 
-    let numberOfBalls = 0;
-    if (canvasWidth > 640) {
-      numberOfBalls = canvasWidth / 144;
-    } else {
-      numberOfBalls = 0;
-    }
-
-    datas
+    data
       .sort(() => 0.5 - Math.random())
-      .splice(0, numberOfBalls)
+      .slice(0, canvasWidth / 90)
       .forEach((App, index) => {
         Composite.add(engine.current.world, [
-          Bodies.circle(canvasWidth / 2, -90 * index, 70, {
-            density: 1,
+          Bodies.circle(canvasWidth / 2, -100 * index, 60, {
+            density: 0.001,
             frictionAir: 0.02,
-            restitution: 0.3,
-            friction: 0.9,
-            speed: 1,
+            frictionStatic: 0.5,
+            restitution: 0.6,
+            friction: 0.1,
             render: {
               sprite: {
                 texture: `/questionImg/${App.imgSrc}`,
-                xScale: 0.583333,
-                yScale: 0.583333,
+                xScale: 0.5,
+                yScale: 0.5,
               },
             },
           }),
@@ -114,7 +110,7 @@ export const Landing = () => {
       mouseConstraint = MouseConstraint.create(engine.current, {
         mouse: mouse,
         constraint: {
-          stiffness: 0.6,
+          stiffness: 0.2,
           render: {
             visible: false,
           },
@@ -151,54 +147,63 @@ export const Landing = () => {
           <canvas ref={canvasRef} />
         </div>
       )}
+      <div className={styles.wrapper}>
+        <div className="flex justify-center items-center h-screen lg:block relative mx-auto ca-pt--home lg:pointer-events-none">
+          <div className="grid lg:grid-cols-10 ca-gap--24">
+            <div className="col-span-10 lg:col-span-6 relative">
+              <div className="flex items-end ca-gap--12 ca-mb--32 select-none">
+                <img
+                  src={LandingLogo}
+                  alt="Ottr Logo"
+                  className={styles.logo}
+                />
+                <span className={clsx(styles.logoTitle, "ca-text--black-80")}>
+                  Presents
+                </span>
+              </div>
 
-      <div className="flex justify-center items-center h-[100vh] lg:block relative container mx-auto sm:pt-[7.5rem] lg:pointer-events-none">
-        <div className="grid lg:grid-cols-12 lg:gap-6">
-          <div className="lg:col-start-2 col-span-12 lg:col-span-6 relative">
-            <div className="flex items-center gap-2 mb-6 lg:mb-8">
-              <img src={LandingLogo} alt="Ottr Logo" className={styles.logo} />
-              <span className={styles.logoTitle}>Presents</span>
-            </div>
+              <div className="ca-mb--24 select-none">
+                <h1 className="ca-heading--one">Cancer Insights Challenge</h1>
+              </div>
 
-            <div className="mb-4 lg:mb-6">
-              <h1 className={styles.Heading}>Cancer Insights Challenge</h1>
-            </div>
+              <div className="ca-mb--48 select-none max-w-[27.625em]">
+                <p className="ca-body--md ca-text--black-80">
+                  Test your cancer knowledge and elevate your understanding of
+                  the disease
+                </p>
+              </div>
 
-            <div className="mb-12 lg:mb-16">
-              <p className={styles.description}>
-                Test your cancer knowledge and elevate your understanding of the
-                disease
-              </p>
-            </div>
-
-            <div className="flex align-center gap-4 pointer-events-auto">
-              <span className="text-base tracking-[0.01rem] leading-[140%] font-medium lg:ca-body--sm">
-                Share
-              </span>
-              <div className="flex gap-6 lg:gap-3">
-                <a className="cursor-pointer" href="https://www.google.com">
-                  <BiLink color={"#470FF4"} size={24} />
-                </a>
-                <a className="cursor-pointer" href="https://www.linkedin.com">
-                  <BiLogoLinkedin color={"#470FF4"} size={24} />
-                </a>
-                <a className="cursor-pointer" href="https://www.facebook.com">
-                  <BiLogoFacebook color={"#470FF4"} size={24} />
-                </a>
-                <a className="cursor-pointer" href="https://www.discord.com">
-                  <BiLogoDiscordAlt color={"#470FF4"} size={24} />
-                </a>
+              <div className="flex align-center ca-gap--16 pointer-events-auto select-none">
+                <span className="ca-body--sm">Share</span>
+                <div className="flex ca-gap--12">
+                  <a className="cursor-pointer" href="https://www.google.com">
+                    <BiLink className={styles.social} />
+                  </a>
+                  <a className="cursor-pointer" href="https://www.linkedin.com">
+                    <BiLogoLinkedin className={styles.social} />
+                  </a>
+                  <a className="cursor-pointer" href="https://www.facebook.com">
+                    <BiLogoFacebook className={styles.social} />
+                  </a>
+                  <a className="cursor-pointer" href="https://www.discord.com">
+                    <BiLogoDiscordAlt className={styles.social} />
+                  </a>
+                </div>
+              </div>
+              <div className={styles.arrow}>
+                <img
+                  src={LandingpageArrow}
+                  className={styles.arrow__img}
+                  alt="arrow"
+                />
               </div>
             </div>
-            <div className={styles.arrow}>
-              <img src={LandingpageArrow} alt="arrow" />
-            </div>
-          </div>
 
-          <div className="col-start-9 col-span-3 mt-14 lg:mt-36 pointer-events-auto">
-            <button className={styles.primaryButton} onClick={changePage}>
-              Take the quiz
-            </button>
+            <div className="col-start-8 col-span-3 mt-14 lg:mt-auto pointer-events-auto">
+              <button className={styles.primaryButton} onClick={startQuiz}>
+                Take the quiz
+              </button>
+            </div>
           </div>
         </div>
       </div>
