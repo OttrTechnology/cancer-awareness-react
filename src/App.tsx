d7a894a4b0d1f9865ab11a098029ad2b1e97eed7
rubek <rubek@ottr.com.np>
2023-09-14 13:48:20 +0545

    🎨 Style: add spaces to separate code chunks

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
-import { Landing, Quiz, GameOver } from "screens";
-import { useGameContext } from "hooks";
 import { useEffect, useRef } from "react";
 import { gsap } from "gsap";
+import { clsx } from "clsx";
+import { Landing, Quiz, GameOver } from "screens";
+import { useGameContext } from "hooks";
 
 const App = () => {
   const { activeScreen } = useGameContext();
+
   const mainRef = useRef<HTMLDivElement>(null);
+
   const landingRef = useRef<HTMLDivElement>(null);
   const quizRef = useRef<HTMLDivElement>(null);
   const gameOverRef = useRef<HTMLDivElement>(null);
@@ -14,29 +17,28 @@ const App = () => {
     const ctx = gsap.context(() => {
       switch (activeScreen) {
         case "LANDING": {
-          const tl = gsap.timeline({
-            defaults: { duration: 1 },
-          });
+          const tl = gsap.timeline({ defaults: { duration: 1 } });
+
           tl.to(landingRef.current, { display: "block", duration: 0 }, ">")
             .to(landingRef.current, { autoAlpha: 1 }, ">")
-            .to(quizRef.current, {
-              autoAlpha: 0,
-            })
-            .to(gameOverRef.current, {
-              autoAlpha: 0,
-            });
+            .to(quizRef.current, { autoAlpha: 0 })
+            .to(gameOverRef.current, { autoAlpha: 0 });
+
           break;
         }
         case "QUIZ": {
           const tl = gsap.timeline({ defaults: { duration: 0.5 } });
+
           tl.to(landingRef.current, { autoAlpha: 0 }, "transitToQuiz")
             .to(landingRef.current, { display: "none", duration: 0 }, ">")
             .to(quizRef.current, { autoAlpha: 1 }, "transitToQuiz")
             .to(quizRef.current, { display: "block", duration: 0 }, "<");
+
           break;
         }
         case "GAME_OVER": {
           const tl = gsap.timeline({ defaults: { duration: 0.5 } });
+
           tl.to(landingRef.current, { autoAlpha: 0 }, "transitToGameOver")
             .to(landingRef.current, { display: "none", duration: 0 }, "<")
             .to(
@@ -47,18 +49,18 @@ const App = () => {
             .to(quizRef.current, { display: "none", duration: 0 }, ">")
             .to(gameOverRef.current, { display: "block", duration: 0 }, ">")
             .to(gameOverRef.current, { autoAlpha: 1 }, ">");
+
           break;
         }
-        default:
-          break;
       }
     }, mainRef);
 
-    return () => ctx.revert(); // cleanup
+    return () => ctx.revert();
   }, [activeScreen]);
+
   return (
     <div
-      className={activeScreen === "GAME_OVER" ? "ca-gameOver" : ""}
+      className={clsx(activeScreen === "GAME_OVER" && "ca-gameOver")}
       ref={mainRef}
     >
       <div ref={landingRef} className="hidden">
--- src/screens/GameOver/components/GameOver.tsx
+++ src/screens/GameOver/components/GameOver.tsx
@@ -1,34 +1,30 @@
-import gameover from "assets/resultEmoji/gameover.svg";
-import gsap from "gsap";
-import { useGameContext } from "hooks";
 import { useEffect, useRef } from "react";
+import gsap from "gsap";
+import gameOver from "assets/resultEmoji/gameover.svg";
+import { useGameContext } from "hooks";
 import styles from "./GameOver.module.scss";
 
 export const GameOverAnimation = () => {
-  const gameOverRef = useRef(null);
   const { remainingLives } = useGameContext();
+
+  const gameOverRef = useRef(null);
 
   useEffect(() => {
     const ctx = gsap.context(() => {
       gsap.fromTo(
         gameOverRef.current,
-        {
-          autoAlpha: 1,
-        },
-        {
-          autoAlpha: 0,
-          duration: 1,
-          delay: 2,
-        }
+        { autoAlpha: 1 },
+        { autoAlpha: 0, duration: 1, delay: 2 }
       );
     });
 
     return () => ctx.revert();
   }, [remainingLives]);
+
   return (
     <>
       <div className={styles.gameOver} ref={gameOverRef}>
-        <img className={styles.gameOverImg} src={gameover} />
+        <img className={styles.gameOverImg} src={gameOver} />
         <div className="ca-heading--one">Game Over !</div>
       </div>
     </>
