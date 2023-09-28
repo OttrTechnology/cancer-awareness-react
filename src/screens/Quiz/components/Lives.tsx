57e3ff0c149b79e64e78298d8944ddfb14c390d6
Nirwan Rai <nirwanrai6969@gmail.com>
2023-09-28 09:25:10 +0545

    ⚡️ Pref: removed unwanted gsap operation by changing fill color of svg in heart

--- src/screens/Quiz/components/Lives.tsx
+++ src/screens/Quiz/components/Lives.tsx
@@ -7,61 +7,42 @@ const { life, lifeLost } = styles;
 
 export const Lives = () => {
   const { remainingLives, totalLives } = useGameContext();
-  const livesContainerRef = useRef(null);
   const droppingLivesContainerRef = useRef(null);
 
   useEffect(() => {
     const ctx = gsap.context(() => {
-      const tl = gsap.timeline();
       if (remainingLives === 2) {
-        tl.to(`[data-effect=life-2]`, { fill: lifeLost, duration: 0.3 }).to(
-          `[data-effect=droppingLife-2]`,
-          {
-            y: "100%",
-            duration: 0.3,
-            autoAlpha: 0,
-            rotate: 45,
-            ease: Cubic.easeIn,
-          }
-        );
-      } else if (remainingLives === 1) {
-        gsap.set(`[data-effect=life-2]`, {
-          fill: lifeLost,
+        gsap.to(`[data-effect=droppingLife-2]`, {
+          y: "100%",
+          duration: 0.3,
+          autoAlpha: 0,
+          rotate: 45,
+          ease: Cubic.easeIn,
         });
-
+      } else if (remainingLives === 1) {
         gsap.set(` [data-effect=droppingLife-2]`, {
           autoAlpha: 0,
         });
 
-        tl.to(`[data-effect=life-1]`, { fill: lifeLost, duration: 0.3 }).to(
-          `[data-effect=droppingLife-1]`,
-          {
-            y: "100%",
-            duration: 0.3,
-            autoAlpha: 0,
-            rotate: 45,
-            ease: Cubic.easeIn,
-          }
-        );
+        gsap.to(`[data-effect=droppingLife-1]`, {
+          y: "100%",
+          duration: 0.3,
+          autoAlpha: 0,
+          rotate: 45,
+          ease: Cubic.easeIn,
+        });
       } else if (remainingLives === 0) {
         gsap.set(`[data-effect=droppingLife-2], [data-effect=droppingLife-1]`, {
           autoAlpha: 0,
         });
 
-        gsap.set(`[data-effect=life-2], [data-effect=life-1]`, {
-          fill: lifeLost,
+        gsap.to(`[data-effect=droppingLife-0]`, {
+          y: "100%",
+          duration: 0.3,
+          autoAlpha: 0,
+          rotate: 45,
+          ease: Cubic.easeIn,
         });
-
-        tl.to(`[data-effect=life-0]`, { fill: lifeLost, duration: 0.3 }).to(
-          `[data-effect=droppingLife-0]`,
-          {
-            y: "100%",
-            duration: 0.3,
-            autoAlpha: 0,
-            rotate: 45,
-            ease: Cubic.easeIn,
-          }
-        );
       }
     });
 
@@ -70,7 +51,7 @@ export const Lives = () => {
 
   return (
     <>
-      <div className="flex items-start" ref={livesContainerRef}>
+      <div className="flex items-start">
         {Array(totalLives)
           .fill(0)
           .map((_, index) => (
@@ -83,10 +64,9 @@ export const Lives = () => {
             >
               <g id="si:Heart">
                 <path
-                  data-effect={`life-${index}`}
                   id="v"
                   d="M27.8261 5C24.4203 5 21.4782 6.995 20 9.905C18.5217 6.995 15.5797 5 12.1739 5C7.28984 5 3.33331 9.095 3.33331 14.135C3.33331 19.175 6.3623 23.795 10.2753 27.59C14.1884 31.385 20 35 20 35C20 35 25.6232 31.445 29.7246 27.59C34.1014 23.48 36.6666 19.19 36.6666 14.135C36.6666 9.08 32.7101 5 27.8261 5Z"
-                  fill={life}
+                  fill={lifeLost}
                 />
               </g>
             </svg>
